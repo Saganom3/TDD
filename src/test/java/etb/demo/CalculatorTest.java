833ca2f704ea8bff589715a9c4ac3dd15c1031f2
Oscar Vallejo <oscar.vallejo@neoris.com>
2022-06-01 19:40:48 -0500

    init

---
+++ src/test/java/etb/demo/CalculatorTest.java
@@ -0,0 +1,28 @@
+package etb.demo;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class CalculatorTest {
+    private Calculator calculator;
+
+    @BeforeEach
+    void setUp() {
+        calculator=new Calculator();
+    }
+
+    @Test
+    public void testSumPositiveValues_withNegativeValues() {
+        int result = calculator.sumPositiveValues(-10, -20, -30);
+        assertEquals(result, 0);
+    }
+
+    @Test
+    void testSumPositiveCalues_whithPositiveValues() {
+        int result = calculator.sumPositiveValues(10, 20, 30);
+        assertEquals(result,60);
+    }
+}
